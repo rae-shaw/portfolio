b39890d9acdc5fb02a30a5d0d690f9a7a2b73c31
Rachael Shaw <rachaelleeshaw@gmail.com>
2019-07-10 13:33:41 -0500

    added-more-css

---
+++ index.js
@@ -0,0 +1,11 @@
+//responsible for changing the image icon on mouseover
+function imageMouseOver(){
+	$("#cornermouseover").mouseover(function(){
+  console.log("mouseover");
+});
+}
+
+
+//responsible for expanding the hamburger nav
+function hamburgerMenu
+
