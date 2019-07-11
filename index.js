3270e9efd5f2d1217b784b6c5636d5a5659abd9d
Rachael Shaw <rachaelleeshaw@gmail.com>
2019-07-11 13:16:44 -0500

    new-css-and-js

--- index.js
+++ index.js
@@ -7,5 +7,47 @@ function imageMouseOver(){
 
 
 //responsible for expanding the hamburger nav
-function hamburgerMenu
+function expandMenu(){
+	$("div.menu").toggleClass("menu_show");
+	}
+
+function animateBars(){
+  $('.container').toggleClass("change");
+}
+
+/*function collapseMenu(){
+		$('body').on('click', '.container', function(){
+			$("menu_show").toggleClass("div.menu");
+			console.log('click2 ran!')
+	});
+}*/
+
+function hamburgerClick(){
+	$('.nav').on('click', '.container', function(event){
+		event.preventDefault();
+		console.log('hamburger clicked!');
+		expandMenu();
+		animateBars();
+		//collapseMenu();
+	});
+}
+
+function menuItemClick(){
+	$('.nav').on('click', '.menu', function(event){
+		//event.preventDefault();
+		expandMenu();
+		animateBars();
+});
+}
+
+
+function loadPage () {
+  hamburgerClick();
+  menuItemClick();
+  console.log('load page!');
+
+}
+
+$(loadPage);
+
 
